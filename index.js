9f22e1e3b3c68956435e69c9f3a9d425c4fabd1b
dimsy453 <149811120+dimsy453@users.noreply.github.com>
2024-01-27 20:13:41 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("hello from TS10");
+// const func = (name:string) => {
+//     name.
+// };
+//
+// class User {
+//   name: string;
+//   age: number;
+// }
---
+++ index.ts
@@ -0,0 +1,10 @@
+console.log(`hello from TS10`);
+// const func = (name:string) => {
+//     name.
+// };
+//
+// class User {
+//   name: string;
+//   age: number;
+// }
+
